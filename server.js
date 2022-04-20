56a25ca55d5eb3cc99bb7f832ff0fc2854731dbb
saharok <97031014+1Saharok1@users.noreply.github.com>
2022-04-20 06:16:40 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use("/", require("./routes/home"));
+app.use("/about", require("./routes/about"));
+app.use("/profile", require("./routes/profile"));
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
