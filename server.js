778df1967ac40a4ea3e5ce128c44883d01bd5a5b
saharok <97031014+1Saharok1@users.noreply.github.com>
2022-04-20 10:10:10 +0600

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.use("/", require("./routes/home"));
-app.use("/about", require("./routes/about"));
-app.use("/profile", require("./routes/profile"));
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
