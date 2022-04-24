967a1670584d1ed6bfc21b112129198f146a2e2d
1Saharok1 <ayana08062004@gmail.com>
2022-04-24 15:44:55 +0600

    backend

--- routes/gallery.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router
-    .route("/")
-    .get((req, res) => res.sendFile(process.cwd()+"/gallery.html"))
-    .post((req, res) => res.send("POST GALLERY"));
-module.exports = router;
--- routes/services.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router
-    .route("/")
-    .get((req, res) => res.sendFile(process.cwd()+"/services.html"))
-    .post((req, res) => res.send("POST SERVICES"));
-module.exports = router;
